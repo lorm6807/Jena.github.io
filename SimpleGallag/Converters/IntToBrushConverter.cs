66cac80e47d16cec01c2bb2d393527a4ec7a6d74
jena <lorm6807@gmail.com>
2022-10-10 22:38:26 +0900

    왼쪽 오른쪽 바인딩 쫌쫌따리 구현

---
+++ SimpleGallag/Converters/IntToBrushConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace SimpleGallag.Converters
+{
+    public class IntToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int xPos = (int)value;
+            int parm = Int32.Parse(parameter.ToString());
+
+            if (xPos == parm)
+                return Brushes.Red;
+            else
+                return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
--- SimpleGallag/ViewModels/MainViewModel.cs
+++ SimpleGallag/ViewModels/MainViewModel.cs
@@ -1,12 +1,58 @@
-﻿using System;
+﻿using Common.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SimpleGallag.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel : ViewModelBase
     {
+        public MainViewModel()
+        {
+        }
+
+        private bool isGaming;
+        public bool IsGaming
+        {
+            get => isGaming;
+            set => Set(ref isGaming, value);
+        }
+
+        private int columnCount = 5;
+        public int ColumnCount
+        {
+            get => columnCount;
+            set => Set(ref columnCount, value);
+        }
+
+        private int tankPosition = 2;
+        public int TankPosition
+        {
+            get => tankPosition;
+            set => Set(ref tankPosition, value);
+        }
+
+        private ICommand leftDownCommand;
+        public ICommand LeftDownCommand => leftDownCommand ?? (leftDownCommand = new RelayCommand(LeftDownAction));
+        public void LeftDownAction()
+        {
+            //if (!isGaming)
+            //    return;
+            if (tankPosition > 0)
+                TankPosition = tankPosition - 1;
+        }
+
+        private ICommand rightDownCommand;
+        public ICommand RightDownCommand => rightDownCommand ?? (rightDownCommand = new RelayCommand(RightDownAction));
+        public void RightDownAction()
+        {
+            //if (!isGaming)
+            //    return;
+            if (tankPosition < ColumnCount - 1)
+                TankPosition = tankPosition + 1;
+        }
     }
 }
